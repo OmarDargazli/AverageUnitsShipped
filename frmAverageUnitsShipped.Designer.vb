2115a9277cc42ff26da09adabafdd4add0b5be45
OmarDargazli <36515009+OmarDargazli@users.noreply.github.com>
2018-03-05 16:10:42 -0500

    Initial commit

---
+++ frmAverageUnitsShipped.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAverageUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.AverageUnitsShippedToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.tbUnitInput = New System.Windows.Forms.TextBox()
+        Me.lbAverageOutput = New System.Windows.Forms.Label()
+        Me.tbEntryDisplay = New System.Windows.Forms.TextBox()
+        Me.lbUnits = New System.Windows.Forms.Label()
+        Me.lbDayCounter = New System.Windows.Forms.Label()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(12, 242)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnter.TabIndex = 5
+        Me.btnEnter.Text = "&Enter"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.btnEnter, "Enter the number for this day")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(197, 242)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.btnExit, "Exit the program")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(104, 242)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 6
+        Me.btnReset.Text = "&Reset"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.btnReset, "Reset the enter program")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'tbUnitInput
+        '
+        Me.tbUnitInput.Location = New System.Drawing.Point(104, 25)
+        Me.tbUnitInput.Name = "tbUnitInput"
+        Me.tbUnitInput.Size = New System.Drawing.Size(75, 20)
+        Me.tbUnitInput.TabIndex = 1
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.tbUnitInput, "Enter a number for this day")
+        '
+        'lbAverageOutput
+        '
+        Me.lbAverageOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbAverageOutput.Location = New System.Drawing.Point(65, 216)
+        Me.lbAverageOutput.Name = "lbAverageOutput"
+        Me.lbAverageOutput.Size = New System.Drawing.Size(160, 23)
+        Me.lbAverageOutput.TabIndex = 4
+        Me.lbAverageOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.lbAverageOutput, "Will output the average units after 7 days")
+        '
+        'tbEntryDisplay
+        '
+        Me.tbEntryDisplay.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.tbEntryDisplay.Location = New System.Drawing.Point(65, 72)
+        Me.tbEntryDisplay.Multiline = True
+        Me.tbEntryDisplay.Name = "tbEntryDisplay"
+        Me.tbEntryDisplay.ReadOnly = True
+        Me.tbEntryDisplay.Size = New System.Drawing.Size(160, 127)
+        Me.tbEntryDisplay.TabIndex = 3
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.tbEntryDisplay, "Will display entries")
+        '
+        'lbUnits
+        '
+        Me.lbUnits.Location = New System.Drawing.Point(12, 25)
+        Me.lbUnits.Name = "lbUnits"
+        Me.lbUnits.Size = New System.Drawing.Size(75, 23)
+        Me.lbUnits.TabIndex = 0
+        Me.lbUnits.Text = "&Units:"
+        Me.lbUnits.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.lbUnits, "How many units were sold today?")
+        '
+        'lbDayCounter
+        '
+        Me.lbDayCounter.AutoSize = True
+        Me.lbDayCounter.Location = New System.Drawing.Point(194, 30)
+        Me.lbDayCounter.Name = "lbDayCounter"
+        Me.lbDayCounter.Size = New System.Drawing.Size(35, 13)
+        Me.lbDayCounter.TabIndex = 2
+        Me.lbDayCounter.Text = "Day 1"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.lbDayCounter, "Displays the current day")
+        '
+        'frmAverageUnitsShipped
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(284, 277)
+        Me.Controls.Add(Me.lbDayCounter)
+        Me.Controls.Add(Me.tbEntryDisplay)
+        Me.Controls.Add(Me.lbAverageOutput)
+        Me.Controls.Add(Me.tbUnitInput)
+        Me.Controls.Add(Me.lbUnits)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnEnter)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmAverageUnitsShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me, "Enter 7 numbers for each day of the week and the program will display the average" &
+        " of those numbers")
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents AverageUnitsShippedToolTip As ToolTip
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents lbUnits As Label
+    Friend WithEvents tbUnitInput As TextBox
+    Friend WithEvents lbAverageOutput As Label
+    Friend WithEvents tbEntryDisplay As TextBox
+    Friend WithEvents lbDayCounter As Label
+End Class
---
+++ frmAverageUnitsShipped.vb
@@ -0,0 +1,86 @@
+﻿'''Author:                  Omar Dargazli, 100662836
+'''Date:                    February 7, 2017
+'''Program:                 AverageUnitsShipped
+'''Description:             Form will take 7 days of user input and return
+'''                         the average of the units entered
+
+Public Class frmAverageUnitsShipped
+    'Constant Declarations
+    Const unitMaximum As Integer = 1000     'This is the upper boundary of the valid range
+    Const unitMinimum As Integer = 0        'This is the lower boundary of the valid range
+    Const daysOfWeek As Integer = 6         'This is the upper boundary of the week
+
+
+    'Variable Declarations
+    Dim unitInput As String = String.Empty  'Holding the unit as a string
+    Dim unitInputValid As Integer = 0       'Holds the unit after it has been validated
+    Dim unitArray(6) As Integer             'Array will contain the units entered by the user
+    Dim dayCounter As Integer = 0           'Counter to display the days and keep track of units entered
+    Dim totalUnits As Integer = 0           'Holds the total amount of units added and the average of the units
+
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        If Not (dayCounter > daysOfWeek) Then                                                                   'Checking to see that the dayCounter does not exceed the amount of days
+
+            If (dayCounter <= daysOfWeek) Then                                                                  'Checking to see that the dayCounter is within valid range
+                unitInput = tbUnitInput.Text                                                                    'Get the input from the textbox
+
+                If Not Integer.TryParse(unitInput, unitInputValid) Then                                         'Checking to see that the input is a number
+                    MessageBox.Show("Please ensure you enter a number for units shipped!")                      'Display error message
+                    tbUnitInput.SelectionStart = 0                                                              'Start the selection at the beginning of the textbox
+                    tbUnitInput.SelectionLength = tbUnitInput.Text.Length                                       'Select the rest of the text in the textbox
+
+                ElseIf (unitInputValid < unitMinimum OrElse unitInputValid > unitMaximum) Then                  'Checking to see that the input is in range
+                    MessageBox.Show("Please ensure you enter a number in range for units shipped!")             'Display error message
+                    tbUnitInput.SelectionStart = 0                                                              'Start the selection at the beginning of the textbox
+                    tbUnitInput.SelectionLength = tbUnitInput.Text.Length                                       'Select the rest of the text in the texbox
+                Else
+                    unitArray(dayCounter) = unitInputValid                                                      'Store the unitInput when it's valid in array at the current index defined by dayCounter
+                    tbEntryDisplay.Text += unitArray(dayCounter).ToString & vbCrLf                              'Display the input along with past inputs in the textbox
+
+                    dayCounter = dayCounter + 1                                                                 'Counter increment
+                    If dayCounter > daysOfWeek Then                                                             'Checking to see if the counter exceeds the days of the week
+                        tbUnitInput.ReadOnly = True                                                             'Disable user's ability to enter anything in the textbox
+                        btnEnter.Enabled = False                                                                'Disable user's ability to click the accept button
+
+
+                        For arrayCounter As Integer = unitMinimum To dayCounter - 1                             'For counter to add all units together in the array
+                            totalUnits += unitArray(arrayCounter)                                               'Add the element of the array to totalUnits
+
+                        Next
+                        totalUnits = Math.Round((totalUnits / unitArray.Length), 2)                             'Caculates the average of the total units
+                        lbAverageOutput.Text = "Unit Average for the Week: " + totalUnits.ToString              'Displays the average in the lbAverageOutput label
+                    End If
+                End If
+
+                tbUnitInput.Text = ""                                                                           'Clears the text for next input
+                tbUnitInput.Focus()                                                                             'Focuses on the textbox for next input
+
+                If (dayCounter <= daysOfWeek) Then                                                              'Checking to see the dayCounter does not exceed the days of the week
+                    lbDayCounter.Text = "Day " + (dayCounter + 1).ToString                                      'Display the current day in the lbDayCounter
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        formReset()                                                                                             'Calls the reset function 
+    End Sub
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()                                                                                      'Closes the form
+    End Sub
+    Private Sub formReset()
+        dayCounter = 0                                                                                          'Reset the dayCounter
+        totalUnits = 0                                                                                          'Reset the totalUnits
+        btnEnter.Enabled = True                                                                                 'Reenable the Enter button
+        tbUnitInput.Text = String.Empty                                                                         'Make the unit input textbox empty 
+        tbUnitInput.ReadOnly = False                                                                            'Give the user the ability to type in the textbox
+        tbUnitInput.Focus()                                                                                     'Focus on the textbox
+        tbEntryDisplay.Text = String.Empty                                                                      'Make the entry display textbox empty
+        lbAverageOutput.Text = String.Empty                                                                     'Make the average output label empty
+        lbDayCounter.Text = "Day 1"                                                                             'Reset the day counter label to the first day
+    End Sub
+End Class
